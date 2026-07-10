27a4be646fb16da57bb805dffba7d3108ad8521b
Buddhastre <ttngls+gitlab@proton.me>
2026-07-10 11:19:20 +0200

    renamed src into banwords

---
+++ banwords/main.py
@@ -0,0 +1,7 @@
+from banwords import logger
+from banwords.cli import cli_args
+
+
+logger.info("checking CLI...")
+args = cli_args()
+
--- src/main.py
+++
@@ -1,7 +0,0 @@
-from src import logger
-from src.cli import cli_args
-
-
-logger.info("checking CLI...")
-args = cli_args()
-
