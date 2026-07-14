52b99bd8835e98491b508a3df46ece9b616041d2
Buddhastre <ttngls+gitlab@proton.me>
2026-07-14 18:12:21 +0200

    arrange

--- src/banwords/cli.py
+++ src/banwords/cli.py
@@ -4,10 +4,19 @@ from logging import INFO, DEBUG, _levelToName
 
 log_levels: list[str] = [_levelToName[INFO], _levelToName[DEBUG]]
 
+
 def cli_args() -> Namespace:
 
-    parser = ArgumentParser(prog="banwords CLI", formatter_class=ArgumentDefaultsHelpFormatter)
+    parser = ArgumentParser(
+        prog="banwords CLI", formatter_class=ArgumentDefaultsHelpFormatter
+    )
 
     parser.add_argument("-c", "--conf", default="pyproject.toml", help="tool's conf")
-    parser.add_argument("-l", "--loglevel", choices=log_levels, default=_levelToName[INFO], help="lvl your logs")
+    parser.add_argument(
+        "-l",
+        "--loglevel",
+        choices=log_levels,
+        default=_levelToName[INFO],
+        help="lvl your logs",
+    )
     return parser.parse_args()
