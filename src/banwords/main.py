ec4d042fbac401b1bb71022dfe7493d27c3eadcb
Buddhastre <ttngls+gitlab@proton.me>
2026-07-14 18:28:53 +0200

    set loglevel with args

--- src/banwords/main.py
+++ src/banwords/main.py
@@ -1,10 +1,9 @@
 from banwords import logger
 from banwords.cli import cli_args
 
-logger.setLevel(10)
 
-logger.info("checking CLI")
 args = cli_args()
+logger.setLevel(args.loglevel)
 logger.debug(args.__dict__)
 
 def entry() -> None:
