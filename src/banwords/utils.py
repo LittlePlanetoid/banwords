0f34ef51368e5e5ff07c25850e4d0c3d0104875c
Buddhastre <ttngls+gitlab@proton.me>
2026-07-14 19:13:25 +0200

    add try/except to set errors properly

--- src/banwords/utils.py
+++ src/banwords/utils.py
@@ -6,14 +6,20 @@ from typing import Optional
 
 
 def _parse_tomfile(path: str) -> dict:
-    with open(path, "rb") as tfile:
-        return tomllib.load(tfile)
+    try:
+        with open(path, "rb") as tfile:
+            return tomllib.load(tfile)
+    except FileNotFoundError:
+        raise FileNotFoundError("conf file was not found")
 
 def get_banwords_conf(path: str) -> Optional[dict]:
     conf = _parse_tomfile(path)
-    if conf["tool"]["banwords"]:
-        logger.debug(f"banword's conf found -> {conf["tool"]["banwords"]}")
-        return conf["tool"]["banwords"]
+    try:
+        if conf["tool"]["banwords"]:
+            logger.debug(f"banword's conf found -> {conf["tool"]["banwords"]}")
+            return conf["tool"]["banwords"]
+    except KeyError:
+        logger.error("no tool.banwords found")
     
-    logger.error("banwords's conf not found!")
+    logger.error(f"banwords's conf not found into {path}")
     return None
