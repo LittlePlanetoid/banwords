0fe018841fbfa114bfa1b77066671d2335fcf4ea
Buddhastre <ttngls+gitlab@proton.me>
2026-07-14 20:20:42 +0200

    files's linted

--- src/banwords/cli.py
+++ src/banwords/cli.py
@@ -11,7 +11,13 @@ def cli_args() -> Namespace:
         prog="banwords CLI", formatter_class=ArgumentDefaultsHelpFormatter
     )
 
-    parser.add_argument("-c", "--conf", default="pyproject.toml", help="tool's conf", choices=["pyproject.toml", "banwords.toml"])
+    parser.add_argument(
+        "-c",
+        "--conf",
+        default="pyproject.toml",
+        help="tool's conf",
+        choices=["pyproject.toml", "banwords.toml"],
+    )
     parser.add_argument(
         "-l",
         "--loglevel",
