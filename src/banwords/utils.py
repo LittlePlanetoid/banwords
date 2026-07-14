17857fe978004d0543984ce6bc945bed42381a0f
Buddhastre <ttngls+gitlab@proton.me>
2026-07-14 19:06:52 +0200

    debug log for conf

--- src/banwords/utils.py
+++ src/banwords/utils.py
@@ -12,7 +12,7 @@ def _parse_tomfile(path: str) -> dict:
 def get_banwords_conf(path: str) -> Optional[dict]:
     conf = _parse_tomfile(path)
     if conf["tool"]["banwords"]:
-        logger.debug("banword's conf found!")
+        logger.debug(f"banword's conf found -> {conf["tool"]["banwords"]}")
         return conf["tool"]["banwords"]
     
     logger.error("banwords's conf not found!")
