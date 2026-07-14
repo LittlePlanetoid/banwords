03cbb7109ae299e1ebf7a034e1891fa5d1e749e1
Buddhastre <ttngls+gitlab@proton.me>
2026-07-14 19:04:34 +0200

    get conf

--- src/banwords/main.py
+++ src/banwords/main.py
@@ -1,5 +1,6 @@
 from banwords import logger
 from banwords.cli import cli_args
+from banwords.utils import get_banwords_conf
 
 
 args = cli_args()
@@ -7,5 +8,5 @@ logger.setLevel(args.loglevel)
 logger.debug(args.__dict__)
 
 def entry() -> None:
-    ...
+    toml = get_banwords_conf(args.conf)
 
